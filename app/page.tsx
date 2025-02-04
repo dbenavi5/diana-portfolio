34c98fd011214b9cabe1bca7797a29bd4cec5bed
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2025-02-04 03:21:06 -0800

    add intro text section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/sections/header";
 import HeroSection from "@/sections/hero";
+import Intro from "@/sections/intro";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <HeroSection/>
+      <HeroSection />
+      <Intro/>
     </>
   );
 }
