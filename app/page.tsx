3207e4ac4fa8c819db29510eedb775f39104a429
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2025-02-04 14:29:59 -0800

    add project section content

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Header from "@/sections/header";
 import HeroSection from "@/sections/hero";
 import Intro from "@/sections/intro";
+import Projects from "@/sections/projects";
 
 export default function Home() {
   return (
     <>
       <Header />
       <HeroSection />
-      <Intro/>
+      <Intro />
+      <Projects />
     </>
   );
 }
